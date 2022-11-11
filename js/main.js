9ef97bd0a5158591abd44f0e782e97508a855141
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-11 15:17:06 +0100

    Aggiunge milestone e bonus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return{
+            message: "Hello World! I'm Vue.js!",
+            image: "vueImg.png",
+            classList: "pb-20"
+        }
+    }
+}).mount("#app");
